42f3cc644390b6c96b621b6de745114acdd28032
slowikowskiarkadiusz <slowikowskiarkadiusz@gmail.com>
2026-01-11 20:39:19 +0100

    update

---
+++ src/scenes/esp32test/esp32test_scene.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+
+#include "../../actors/rectangle_actor.hpp"
+#include "../../actors/text_actor.hpp"
+#include "esp32test_scene.hpp"
+
+esp32test_scene::esp32test_scene() = default;
+
+void esp32test_scene::init() {
+}
+
+void esp32test_scene::update(float delta_time) {}
+
+void esp32test_scene::fixed_update(float fixed_delta_time) {}
---
+++ src/scenes/esp32test/esp32test_scene.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include "../../engine.hpp"
+
+class esp32test_scene : public scene {
+public:
+    esp32test_scene();
+    void init() override;
+    void update(float delta_time) override;
+    void fixed_update(float fixed_delta_time) override;
+
+private:
+    void update();
+};
